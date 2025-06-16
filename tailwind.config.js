2dadd28795ccc11f9f9dfa350764797705758b8a
ChaitanyaChu <151407543+ChaitanyaChu@users.noreply.github.com>
2025-06-15 19:48:39 -0500

    Deploy ready

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{astro,html,js,jsx,ts,tsx}"],
+  darkMode: 'class', // <-- this enables class-based dark mode
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
